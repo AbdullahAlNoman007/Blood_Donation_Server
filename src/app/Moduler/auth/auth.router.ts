3db287d6f8b5db62f216668e4212d40fe5dc22c7
Sunan13208 <sunanrabbi1918@gmail.com>
2024-06-08 20:02:51 +0600

    auth added

--- src/app/Moduler/auth/auth.router.ts
+++ src/app/Moduler/auth/auth.router.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import validateRequest from '../../middleWare/validationRequest'
 import { changePasswordValidation, loginValidation } from './auth.validation'
 import { authController } from './auth.controller'
+import auth from '../../middleWare/auth'
+import { userRole } from '@prisma/client'
 
 
 const router = express.Router()
 
 router.post('/login', validateRequest(loginValidation), authController.login)
-router.post('/change-password', validateRequest(changePasswordValidation), authController.login)
+router.post('/change-password', auth(userRole.Admin, userRole.Donor, userRole.Requester), validateRequest(changePasswordValidation), authController.login)
 
 export const authRouter = router
